7930814af0127709644b4c87e7145f10d75cb4dd
Nicholas Lau <nicholas.m.lau@gmail.com>
2014-03-25 04:16:58 -0700

    finished sign in and sign out

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -22,6 +22,23 @@ describe "signup" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+      describe "after submission" do
+        before do
+          fill_in "Name",         with: ""
+          fill_in "Email",        with: ""
+          fill_in "Password",     with: ""
+          fill_in "Confirmation", with: ""
+          click_button submit
+        end
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+        it { should have_content("Name can't be blank")}
+        it { should have_content("Email can't be blank")}
+        it { should have_content("Email is invalid")}
+        it { should have_content("Password can't be blank")}
+        it { should have_content("Password is too short (minimum is 6 characters)")}
+      end
     end
 
     describe "with valid information" do
@@ -34,6 +51,18 @@ describe "signup" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
       end
     end
   end
